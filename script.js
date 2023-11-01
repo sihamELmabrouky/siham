d44b252c208e1db92c6cee271da9653604a5508f
sihamELmabrouky <sihamelma2023@gmail.com>
2023-11-01 21:13:15 +0100

    home and about pages

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 menu = document.querySelector(".menu");
 clos = document.querySelector(".close");
 links = document.querySelector(".links");
-
+overlay = document.querySelector(".overlay");
 menu.addEventListener("click", () => {
     // alert()
-    menu.style.display = "none"
-    clos.style.display = "block"
-    links.style.display = "block"
+    menu.style.display = "none";
+    links.style.display = "block";
+    clos.style.display = "block";
+    overlay.style.display = "none";
+
 
 
 });
 clos.addEventListener("click", () => {
     // alert()
-    menu.style.display = "block"
-    clos.style.display = "none"
-    links.style.display = "none"
+    menu.style.display = "block";
+    links.style.display = "none";
+    clos.style.display = "none";
+    overlay.style.display = "none";
+
 
 
 });
+// reveal
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
